cdaee29896026389b40c96f5f10b115d0b62141b
Khuong-PlanB <khuong.pham.minh@plan-b.co.jp>
2026-05-12 15:15:24 +0700

    init lesson 7

---
+++ lession 7/base/src/main/java/com/example/base/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.base.controller;
+
+import com.example.base.base.RestApiV1;
+import com.example.base.base.RestData;
+import com.example.base.base.VsResponseUtil;
+import com.example.base.constant.UrlConstant;
+import com.example.base.domain.dto.request.CreateUserRequestDto;
+import com.example.base.domain.dto.response.UserResponseDto;
+import com.example.base.security.SecurityUtils;
+import com.example.base.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestApiV1
+@RequiredArgsConstructor
+@Validated
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+
+  UserService userService;
+
+  @Operation(summary = "Lấy thông tin profile", description = "Dùng để người dùng lấy thông tin profile đầy đủ", security = @SecurityRequirement(name = "Bearer Token"))
+  @GetMapping(UrlConstant.User.GET_PROFILE)
+  public ResponseEntity<RestData<UserResponseDto>> getMyProfile() {
+    String userId = SecurityUtils.getCurrentUserId();
+    UserResponseDto response = userService.getMyProfile(userId);
+    return VsResponseUtil.success(response);
+  }
+
+  // //
+  // Methods for ADMIN //
+  // //
+
+  @Tag(name = "admin-controller")
+  @Operation(summary = "Lấy thông tin user theo ID", description = "Dùng để admin lấy thông tin chi tiết của một user", security = @SecurityRequirement(name = "Bearer Token"))
+  @GetMapping(UrlConstant.Admin.GET_USER)
+  public ResponseEntity<RestData<UserResponseDto>> getUserById(@PathVariable String userId) {
+    UserResponseDto response = userService.getUserById(userId);
+    return VsResponseUtil.success(response);
+  }
+
+  @Tag(name = "admin-controller")
+  @Operation(summary = "Tạo user mới", description = "Dùng để admin tạo user mới trong hệ thống", security = @SecurityRequirement(name = "Bearer Token"))
+  @PostMapping(UrlConstant.Admin.CREATE_USER)
+  public ResponseEntity<RestData<UserResponseDto>> createUser(@Valid @RequestBody CreateUserRequestDto request) {
+    UserResponseDto response = userService.createUser(request);
+    return VsResponseUtil.success(response);
+  }
+
+}
